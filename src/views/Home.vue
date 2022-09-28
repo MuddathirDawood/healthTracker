539c51880840705ac362d0ed0156ae91f847ad90
MuddathirDawood <dmuddathir@gmail.com>
2022-09-28 12:15:33 +0200

    first container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -47,7 +47,56 @@
                     <v-btn color="teal" rounded text depressed>Year</v-btn>
                     <v-btn text>Month</v-btn>
                   </v-app-bar>
+                  <template>
+                    <v-sparkline
+                    :value="value"
+                    color="teal"
+                    :smooth="radius || false"
+                    :padding="padding"
+                    :line-width="width"
+                    :stroke-linecap="lineCap"
+                    :fill="fill"
+                    :type="type"
+                    :auto-line-width="autoLineWidth"
+                    auto-draw
+                    >
+                    </v-sparkline>
+                  </template>
                 </v-card>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-card class="mx-12 rounded-xl mt-n4" color="teal" dark>
+                  <v-list-item three line>
+                    <v-list-item-content class="pa-2">
+                      <v-list-item-title class="headline mb-1">
+                        Heart Rate <br>
+                        <h2>112 bpm</h2>
+                      </v-list-item-title>
+                    </v-list-item-content>
+                    <v-list-item-avatar tile size="150" class="pr-10">
+                      <v-icon size="100">fas fa-heartbeat</v-icon>
+                    </v-list-item-avatar>
+                  </v-list-item>
+                </v-card>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-card class="mx-12 rounded-xl mt-n12">
+                  <v-app-bar color="rgba(0,0,0,0)" flat class="ma-8">
+                    <h5>Your activity</h5>
+                    <v-spacer></v-spacer>
+                    <v-btn color="teal" rounded dark depressed>Week</v-btn>
+                    <v-btn text>Month</v-btn>
+                  </v-app-bar>
+                  <v-progress-circular rotate="360" size="100" width="15" value="50" color="teal" class="mt-n5 ml-5 mb-2">
+                    50
+                  </v-progress-circular>
+                  <v-progress-circular rotate="360" size="100" width="15" value="70" color="red" class="mt-n5 ml-5 mb-2">
+                    70
+                  </v-progress-circular>
+                  <v-progress-circular rotate="360" size="100" width="15" value="80" color="blue" class="mt-n5 ml-5 mb-2">
+                    80
+                  </v-progress-circular>
+                </v-card>                 
               </v-col>
             </v-row>
           </v-container>
